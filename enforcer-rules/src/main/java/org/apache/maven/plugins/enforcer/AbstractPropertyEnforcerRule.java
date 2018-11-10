662a7268f779ac79c68ba062351169c9673b184c
Karl Heinz Marbaise <khmarbaise@apache.org>
2018-11-10 21:02:54 +0100

    Added cause for debugging.

--- enforcer-rules/src/main/java/org/apache/maven/plugins/enforcer/AbstractPropertyEnforcerRule.java
+++ enforcer-rules/src/main/java/org/apache/maven/plugins/enforcer/AbstractPropertyEnforcerRule.java
@@ -117,8 +117,34 @@ public abstract class AbstractPropertyEnforcerRule
                 regexMessage = getName() + " \"" + getPropertyName() + "\" evaluates to \"" + propValue + "\".  "
                     + "This does not match the regular expression \"" + regex + "\"";
             }
-            throw new EnforcerRuleException( regexMessage );
+            
+            Throwable cause = new Throwable( prettyOut( propValue.toString().getBytes() ) );
+            
+            throw new EnforcerRuleException( regexMessage, cause );
         }
+    }
+
+    public String prettyOut( byte[] msg )
+    {
+        StringBuilder sb = new StringBuilder( msg.length * 2 );
+        for ( int j = 1; j < msg.length + 1; j++ )
+        {
+            if ( j % 16 == 1 || j == 0 )
+            {
+                if ( j != 0 )
+                {
+                    sb.append( System.lineSeparator() );
+                }
+                sb.append( String.format( "%04d  |  ", j / 16 ) );
+            }
+            sb.append( String.format( "%02X ", msg[j - 1] ) );
+            if ( j % 4 == 0 )
+            {
+                sb.append( " " );
+            }
+        }
+        sb.append( System.lineSeparator() );
+        return sb.toString();
     }
 
     /**
